5174a25251ee6b0f8f3971f70df80f68f7c4bfd7
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-06-08 16:28:27 +0100

    use create element instead of short returning

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React, {createContext, useContext} from 'react'
 import ReactDOM from 'react-dom/client'
 
-const Greeting = () => {
+/*const Greeting = () => {
     return <h1>Hello form React</h1>
-}
+}*/
 
+
+const Greeting = () => {
+  return React.createElement("h1",{},"Hi my name is react")
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Greeting/>)
