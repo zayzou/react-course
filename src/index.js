db0dae7917eaed86a10446bc176f888b5267ae70
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-06-09 11:51:12 +0100

    JSX Rules

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React, {createContext, useContext} from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-/*const Greeting = () => {
-    return <h1>Hello form React</h1>
-}*/
-
-
+// JSX Rules
+// return single element always !!!
+// use <React.Fragment> ... </React.Fragment/> or <>...</>
+// camelCase for naming attributes and properties
+// no class attribute but className 
+// must close every element  </>
 const Greeting = () => {
-  return React.createElement("h1",{},"Hi my name is react")
+    return <React.Fragment>
+        <h1 className={"sas"}>Spring is awesom e</h1>
+        <input type="text"/>
+        <h1>Hello form React</h1>
+    </React.Fragment>
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Greeting/>)
