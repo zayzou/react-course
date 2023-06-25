8bc823f76d374a93650775b7210675cc9e7a6b52
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-06-25 14:34:29 +0100

    add parameter and arguments to index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const BookList = () => {
             <Book/>
             <Book/>
             <Book/>
-
         </section>
     )
 }
@@ -16,11 +15,16 @@ const BookList = () => {
 const inlineStyle = {color: '#617d98', fontSize: "0.75rem", marginTop: "0.5rem"};
 let title = "✅Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones";
 let author = <>James Clear</>;
+
+const fun = (par1, para2) => {
+    console.log(par1, para2)
+}
+
+fun("software", "developer")
 const Book = () => {
     return (
         <article className={'book'}>
-            <img src="./images/cover.jpg"
-                 alt="Atomic Habits" srcSet=""/>
+            <img src="./images/cover.jpg" alt={title}/>
             <h2>{title}</h2>
             <h4 style={inlineStyle}> {author}</h4>
         </article>
