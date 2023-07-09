6ebc4b65b54699e77d27b9e8c597544591d42465
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-07-09 14:08:12 +0100

    replace variable with props

--- src/index.js
+++ src/index.js
@@ -2,31 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const BookList = () => {
-    return (
-        <section className={'booklist'}>
-            <Book/>
-            <Book/>
-            <Book/>
-        </section>
-    )
-}
-
 const inlineStyle = {color: '#617d98', fontSize: "0.75rem", marginTop: "0.5rem"};
 let title = "✅Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones";
 let author = <>James Clear</>;
+let image = "./images/cover.jpg"
+const BookList = () => {
+    return (
+        <section className={'booklist'}>
+            <Book author={author} title={title} img={image}/>
+            <Book author={author} title={title} img={image}/>
+            <Book author={author} title={title} img={image}/>
+        </section>
+    )
+
+}
 
 const fun = (par1, para2) => {
     console.log(par1, para2)
 }
 
 fun("software", "developer")
-const Book = () => {
+const Book = (props) => {
+    console.log(props)
     return (
         <article className={'book'}>
-            <img src="./images/cover.jpg" alt={title}/>
-            <h2>{title}</h2>
-            <h4 style={inlineStyle}> {author}</h4>
+            <img src={props.img} alt={props.title}/>
+            <h2>{props.title}</h2>
+            <h4 style={inlineStyle}> {props.author}</h4>
         </article>
     );
 }
