5a384c2da3bfc90811a62baca15cb68581db6efa
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-07-21 14:07:45 +0100

    implement my solution for find book

--- src/index.js
+++ src/index.js
@@ -5,49 +5,41 @@ import './index.css'
 const inlineStyle = {color: '#617d98', fontSize: "0.75rem", marginTop: "0.5rem"};
 
 
-const books = [
-    {
-        id: 1,
-        title: "Atomic Habits",
-        author: "James Clear",
-        image: "./images/cover.jpg"
-    },
-    {
-        id: 2,
-        title: "The 48 Laws of Power",
-        author: " Robert Greene",
-        image: "./images/cover2.jpg"
-    }
-]
+const books = [{
+    id: 1, title: "Atomic Habits", author: "James Clear", image: "./images/cover.jpg"
+}, {
+    id: 2, title: "The 48 Laws of Power", author: " Robert Greene", image: "./images/cover2.jpg"
+}]
 
 
 const BookList = () => {
-    const someVelue = "This value is demonstrating prop drilling"
-    const displayValue = () => alert(someVelue);
-    return (
-        <section className={'booklist'}>
-            {
-                books.map((book) => {
-                    return <Book displayValue={displayValue} {...book} key={book.id}/>
-                })
-            }
-        </section>
-    )
+
+    const getBook = (i) => {
+        const id = parseInt(prompt("give me book id"))
+        const book = books.find((book) => book.id === id);
+        if (book !== undefined) {
+            console.log(book.title)
+        } else {
+            console.log("Book not found")
+        }
+    }
+    return (<section className={'booklist'}>
+        {books.map((book) => {
+            return <Book getBook={getBook} {...book} key={book.id}/>
+        })}
+    </section>)
 
 }
 
 const Book = (props) => {
-    const {author, title, image, displayValue} = props
-    console.log(props)
-    return (
-        <article className={'book'}>
-            <img src={image} alt={title}/>
-            <h2>{title}</h2>
-            <button type="button" onClick={displayValue}>Display value
-            </button>
-            <h4 style={inlineStyle}> {author}</h4>
-        </article>
-    );
+    const {author, title, image, getBook} = props
+    // console.log(props)
+    return (<article className={'book'}>
+        <img src={image} alt={title}/>
+        <h2>{title}</h2>
+        <button type="button" onClick={getBook}>Display value</button>
+        <h4 style={inlineStyle}> {author}</h4>
+    </article>);
 }
 
 
